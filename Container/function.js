4ee2f6d692f02020525b16f98a8ecec8f4ab520e
AlazarBekele <alazarthe70@gmail.com>
2025-01-27 00:01:59 +0300

    try to add class list in display none div

---
+++ Container/function.js
@@ -0,0 +1,12 @@
+
+
+    function AddClass () {
+        
+        humbergerIcon = document.getElementById ("humbergerIcon")
+
+        ContainerPofile = document.getElementById ("ContainerPofile")
+
+
+        ContainerPofile.classList.add ('show')
+
+    }
